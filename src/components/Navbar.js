60726e380e46a8fe382e0aa1322f55f4d45a4d91
oshadawasath <127081726+oshadawasath@users.noreply.github.com>
2024-07-12 16:37:55 +0530

    more reponsive and nav bar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">
           <span style={{ color: '#8BC34A' }}>UNWIND</span> <span style={{ color: '#006400' }}>CABINS</span>
-        </a>        
+        </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -18,16 +18,16 @@ export default function Navbar() {
           <ul className="navbar-nav">
             <li className="nav-item">
               {/* Use Link from react-scroll for smooth scrolling */}
-              <Link to="dicover_cabins_id" smooth={true} duration={500} className='nav-link d-none d-lg-block'> Our Cabins</Link>
+              <Link to="dicover_cabins_id" smooth={true} duration={500} className='nav-link'> Our Cabins</Link>
             </li>
             <li className="nav-item">
-              <Link to="inspired_id" smooth={true} duration={500} className='nav-link d-none d-lg-block'> Get Inspired</Link>
+              <Link to="inspired_id" smooth={true} duration={500} className='nav-link'> Get Inspired</Link>
             </li>
             <li className="nav-item">
-              <Link to="video_id" smooth={true} duration={500} className='nav-link d-none d-lg-block'> Gift Stay</Link>
+              <Link to="video_id" smooth={true} duration={500} className='nav-link'> Gift Stay</Link>
             </li>
             <li className="nav-item">
-              <Link to="details_about_id" smooth={true} duration={500} className='nav-link d-none d-lg-block'> About us</Link>
+              <Link to="details_about_id" smooth={true} duration={500} className='nav-link'> About us</Link>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/signin">
